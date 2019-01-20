8d94b28aeec0594de1df14ce5a258270495ab093
Kaito Arakaki <e185721@ie.u-ryukyu.ac.jp>
2019-01-20 23:45:36 +0900

    second commit(add point)

--- src/jp/ac/uryukyu/ie/e185721/Blackjack.java
+++ src/jp/ac/uryukyu/ie/e185721/Blackjack.java
@@ -1,7 +1,34 @@
 package jp.ac.uryukyu.ie.e185721;
 
+import java.util.Random;
+
+//Solverクラスには、2個以上の自作メソッドを含むこと。後述のクラス2に、1つ以上のユニットテストを書くこと。
 public class Blackjack {
     public static void main(String[] args){
+            Random Toranpu = new Random();
 
-    }
-}
+            int Card = Toranpu.nextInt(13);
+            int Dealer_point = 0;
+            int Player_point = 0;
+            int[] Dealer = new int[30];
+            int[] Player = new int[30];
+
+        for (int i=0;i<2;i++) {
+            Player[i]=Toranpu.nextInt(13)+1;
+            if (Player[i]>=10) {
+                Player[i]=10;
+            }
+            if (Player[i]==1) {
+                Player[i]=11;
+            }
+            Player_point+=Player[i];
+            if (Player_point>=22) {
+                for (int l=0;l<=Player.length;l++) {
+                    if (Player[l]==11) {
+                        Player[l]=1;
+                        Player_point-=10;
+                    }
+                }
+            }
+        }
+        
---
+++ src/jp/ac/uryukyu/ie/e185721/BlackjackTest.java
@@ -0,0 +1,10 @@
+package jp.ac.uryukyu.ie.e185721;
+
+/*import static org.junit.jupiter.api.Assertions.*;
+
+class BlackjackTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}*/

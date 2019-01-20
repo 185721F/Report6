f54f30b0f0e635f717614b575a7c498fb73d957a
Kaito Arakaki <e185721@ie.u-ryukyu.ac.jp>
2019-01-20 23:47:16 +0900

    second commit(continue point)

--- src/jp/ac/uryukyu/ie/e185721/Blackjack.java
+++ src/jp/ac/uryukyu/ie/e185721/Blackjack.java
@@ -31,4 +31,39 @@ public class Blackjack {
                 }
             }
         }
+        for (int i=2;Player_point<22;i++) {
+            System.out.println("現在の点数は"+Player_point+"です。");
+            System.out.println("もう一枚引くなら「ヒット」、引かないなら「スタンド」と入力してください");
+            String str=br.readLine();
+            if (str.equals("ヒット")) {
+                Player[i]=Random.nextInt(13)+1;
+                if (Player[i]>=10) {
+                    Player[i]=10;
+                }
+                if (Player[i]==1) {
+                    Player[i]=11;
+                }
+                Player_point+=Player[i];
+                if(Player_point>=22) {
+                    System.out.println("プレイヤーはバーストしました");
+                    break;
+                }
+            }
+            if (str.equals("スタンド")) break;
+            for (int j=0;Dealer_point<17;j++) {
+                Dealer[j]=Random.nextInt(13)+1;
+                if (Dealer[j]>=10) {
+                    Dealer[j]=10;
+                }
+                if (Dealer[j]==1) {
+                    Dealer[j]=11;
+                }
+                Dealer_point+=Dealer[j];
+                if(Dealer_point>=22) {
+                    System.out.println("ディーラーはバーストしました");
+                    break;
+                }
+            }
+        }
         
+
